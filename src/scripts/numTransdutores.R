9dea6f25df7d35de75a44fa092d4bb36b2eff311
VerasNp <vitorveras01@gmail.com>
2024-03-27 00:16:02 -0300

    Adds some solutions

---
+++ src/scripts/numTransdutores.R
@@ -0,0 +1,3 @@
+raw_data <- read.csv("./data/numTransdutores.csv", header = TRUE, sep = ";")
+# General simple frequency distribution
+print(data.frame(raw_data))
--- src/scripts/offshoreFuga.R
+++ src/scripts/offshoreFuga.R
@@ -1,10 +1,21 @@
-data <- read.csv("./data/offshoreFuga.csv")
-data2 <- as.numeric(data$Segundos)
-
-
+raw_data <- read.csv("./data/offshoreFuga.csv", sep = ";")
+data <- data.frame(raw_data)
+segundos_data <- data$Segundos
+# Steam and leaf plot
+print(stem(segundos_data))
 # tamanho da amostra
-print(length(data2))
+print(length(segundos_data))
 # média amostral
-print(mean(data2))
+print(mean(segundos_data))
 # mediana amostral
-print(median(data2))
+print(median(segundos_data))
+
+minutos_data <- data$Minutos
+# Steam and leaf plot
+print(stem(minutos_data, scale = 0.5))
+# tamanho da amostra
+print(length(minutos_data))
+# média amostral
+print(mean(minutos_data))
+# mediana amostral
+print(median(minutos_data))
